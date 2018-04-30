3e8e49c1fd01e296f3063ae579ec1faf149a7998
beyescay <beyescay@gmail.com>
2018-04-30 00:32:58 -0500

    Working code 3.0 with output logging

--- processing_codes/main.py
+++ processing_codes/main.py
@@ -1,4 +1,3 @@
-import argparse as ARG
 import sys
 
 sys.path.append("../")
@@ -6,48 +5,36 @@ sys.path.append("../")
 from wrangle_data import DataWrangler
 from build_model import BuildAndTrainModel
 from predict_difference import PredictDifference
-import time as T
 
 if __name__ == "__main__":
 
-    parser = ARG.ArgumentParser()
-
-    parser.add_argument("-i", "--input-text-file", default="../data/training_data/11713_movies_raw_data_prof_format.txt",
-                        help="Input text file containing the details of the movies")
-    parser.add_argument("-m", "--mode", type=str, choices=["train", "test"], default="train",
-                        help="The mode in which this script has to run. Options are \"train\" and \"test\". By default "
-                             "it is in train mode")
-    parser.add_argument("--skip-cleaning", action="store_true", help="If provided will skip the data cleaning process,"
-                                                                     "and use the pre-built feature arrays.")
-    args = parser.parse_args()
-    input_text_file = args.input_text_file
-    mode = args.mode
-    skip_cleaning = args.skip_cleaning
-
-    start = T.clock()
+    input_train_text_file = "../data/training_data/11713_movies_raw_data_prof_format.txt"
+    input_test_text_file = "../data/test_data/2929_movies_raw_data_prof_format.txt"
+    mode = "test"
+    skip_cleaning = False
 
     if mode == "train":
         print("\n\n~~~~~~~~~~~~~~~~~~Starting the data wrangling process in {} mode~~~~~~~~~~~~~~~~~~\n\n".format(mode))
-        DataWrangler(input_text_file, mode="train")
+        DataWrangler(input_train_text_file, mode="train")
 
         print("\n\n~~~~~~~~~~~~~~~~~~Building and training the model~~~~~~~~~~~~~~~~~~\n\n""")
         BuildAndTrainModel()
 
         print("\n\n~~~~~~~~~~~~~~~~~~Predicting on the segregated test data set~~~~~~~~~~~~~~~~~~\n\n")
+        DataWrangler(input_test_text_file, mode="test")
         PredictDifference()
 
     elif mode == "test":
         print("\n\n~~~~~~~~~~~~~~~~~~Starting the data wrangling process in {} mode~~~~~~~~~~~~~~~~~~\n\n".format(mode))
-        DataWrangler(input_text_file, mode="test")
+        DataWrangler(input_test_text_file, mode="test")
 
-        print("\n\n~~~~~~~~~~~~~~~~~~Predicting on {}~~~~~~~~~~~~~~~~~~\n\n".format(input_text_file))
+        print("\n\n~~~~~~~~~~~~~~~~~~Predicting on {}~~~~~~~~~~~~~~~~~~\n\n".format(input_test_text_file))
         PredictDifference()
 
     else:
         print("\n\nInvalid mode. Options are \"train\" and \"test\".\n\n")
 
     print("\n\n~~~~~~~~~~~~~~~~~~All processes completed~~~~~~~~~~~~~~~~~~\n\n")
-    print("End to End total time taken: {}\n\n".format(T.clock()-start))
     print("\n\n=============== That's all Folks! ===============\n\n")
 
 
--- processing_codes/predict_difference.py
+++ processing_codes/predict_difference.py
@@ -5,7 +5,7 @@ import time as T
 import numpy as np
 import glob
 import os
-
+import csv
 import sys
 sys.path.append("../")
 
@@ -13,13 +13,15 @@ sys.path.append("../")
 class PredictDifference:
     def __init__(self):
 
+        self.output_csv = "../data/test_data/predicted_vs_actual.csv"
+
         print("Loading the model...")
         self.models = []
         self.model_paths = glob.glob(os.path.join(os.getcwd(), "../data/models", "*.sav"))
         for model in self.model_paths:
             self.models.append(pickle.load(open(model, 'rb')))
 
-        print("Loading the npz files of training data set...")
+        print("Loading the npz files of the data set...")
         self.x_test_features_matrix = load_npz("../data/npz_arrays/X.npz")
         self.y_test_column_matrix = load_npz("../data/npz_arrays/y.npz")
 
@@ -44,3 +46,15 @@ class PredictDifference:
             print("Test mean absolute error for model {}: {}".format(os.path.basename(self.model_paths[idx]), M.mean_absolute_error(self.y_test_column_matrix, y_test_predicted_column_matrix)))
             print("Test median absolute error for model {}: {}".format(os.path.basename(self.model_paths[idx]), M.median_absolute_error(self.y_test_column_matrix, y_test_predicted_column_matrix)))
             print("Test mean squared error for model {}: {}\n".format(os.path.basename(self.model_paths[idx]), M.mean_squared_error(self.y_test_column_matrix, y_test_predicted_column_matrix)))
+
+        with open(self.output_csv, 'w') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(["predicted", "actual"])
+
+            print(self.y_test_column_matrix.shape)
+            for i in range(len(y_test_predicted_column_matrix)):
+                actual = float(self.y_test_column_matrix.A[i][0])
+                csv_writer.writerow([y_test_predicted_column_matrix[i], actual])
+
+
+
